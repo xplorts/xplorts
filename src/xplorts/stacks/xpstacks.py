1053bd988022cd919569682b7fdfafc804973400
xplorts <134138433+xplorts@users.noreply.github.com>
2023-06-15 08:08:49 +0100

    BUG: Fix xpstack filter by widget
    Was still using the old call, passing a view.  To work with newer bokeh,
    now passes source and filter separately since these are no longer
    available from a view.

--- src/xplorts/stacks/xpstacks.py
+++ src/xplorts/stacks/xpstacks.py
@@ -6,16 +6,16 @@ When imported as a module, the function `grouped_stack()` is defined.
 When run from the command line, `stacks` reads data from a `csv` file
 and creates an HTML document that displays an interactive stacked bar chart.
 
-    The `stacks` command reads data from a `csv` file.  The first row 
+    The `stacks` command reads data from a `csv` file.  The first row
     of data defines column names.  The file should include:
         - a column of category names for an independent variable,
-        - a column of category names for a split factor, and 
+        - a column of category names for a split factor, and
         - one or more columns of data values to be plotted as stacked bars
-          against the independent variable.  The data values can include a 
+          against the independent variable.  The data values can include a
           mix of positive and negative values.
-    
+
     An interactive chart is created, with widgets to select one of the
-    split factor category names (from a pulldown list or a slider), and a 
+    split factor category names (from a pulldown list or a slider), and a
     chart with stacked bars for the value columns.  The independent variable
     can be plotted along either the vertical or horizontal axis.
 
@@ -61,7 +61,7 @@ import yaml
 
 ## Imports from this package
 from xplorts.stacks import grouped_stack
-from xplorts.base import (factor_view, iv_dv_figure, 
+from xplorts.base import (factor_view, iv_dv_figure,
                           link_widgets_to_groupfilters, set_output_file, variables_cmap)
 from xplorts.slideselect import SlideSelect
 
@@ -70,16 +70,16 @@ from xplorts.slideselect import SlideSelect
 def _parse_args():
     """
     Parse command line arguments
-    
+
     Returns
     -------
     `argparse.Namespace` object
-    
+
     Examples
     --------
     args = _parse_args()
     data = pd.read_csv(args.datafile)
-    
+
     Resources
     ---------
     [argparse — Parser for command-line options, arguments and sub-commands](https://docs.python.org/3/library/argparse.html#dest)
@@ -89,7 +89,7 @@ def _parse_args():
         prog="python -m xplorts.stacks",
         description="Create interactive stacked bars for data series with a split factor"
     )
-    parser.add_argument("datafile", 
+    parser.add_argument("datafile",
                         help="Name of .csv file with data series and split factor")
     parser.add_argument("-b", "--by", type=str,
                         help="Name of factor variable for splits")
@@ -101,18 +101,18 @@ def _parse_args():
     iv_group.add_argument("-y", type=str,
                           help="Name of independent variable, for vertical axis")
 
-    parser.add_argument("-v", "--values", 
+    parser.add_argument("-v", "--values",
                         nargs="+", type=str,
                         help="Dependent variables to show as stacked bars")
-    parser.add_argument("-g", "--args", 
+    parser.add_argument("-g", "--args",
                         type=str,
                         help="Keyword arguments for grouped_stack(), specified as YAML mapping")
 
-    
-    parser.add_argument("-t", "--save", type=str, 
+
+    parser.add_argument("-t", "--save", type=str,
                         help="Name of interactive .html to save, if different from the datafile base")
 
-    parser.add_argument("-s", "--show", action="store_true", 
+    parser.add_argument("-s", "--show", action="store_true",
                         help="Show interactive .html")
     args = parser.parse_args()
 
@@ -128,7 +128,7 @@ def main():
     print(args)
 
     data = pd.read_csv(args.datafile, dtype=str)
-    
+
     # Unpack args specifying which columns to use.
     if all(getattr(args, arg) is None for arg in ["x", "y", "by", "values"]):
         # Get datevar from first column, byvar from second, values from remaining.
@@ -145,15 +145,15 @@ def main():
         else:
             iv_axis = "y"
             iv_variable = args.y
-    
+
     title = "stacks: " + Path(args.datafile).stem
-    
+
     # Configure output file for interactive html.
     set_output_file(
         args.save or args.datafile,
         title = title
     )
-    
+
     # Make a slide-select widget to choose factor level.
     widget = SlideSelect(options=list(data[byvar].unique()),
                          name=byvar + "_select")
@@ -161,9 +161,10 @@ def main():
     source = ColumnDataSource(data)
     view_by_factor = factor_view(source, byvar)
 
-    link_widgets_to_groupfilters(widget, 
-                                 view=view_by_factor)
-    
+    link_widgets_to_groupfilters(widget,
+                                 source=source,
+                                 filter=view_by_factor.filters)
+
     # Map variables to colors.
     default_color_map = variables_cmap(datavars,
                                        "Category10_10")
@@ -188,14 +189,14 @@ def main():
         view=view_by_factor,
         color=default_bar_colors,
         **args.args)
-    
+
     # Make app that shows widget and chart.
     app = layout([
         Div(text="<h1>" + title),  # Show title as level 1 heading.
         [widget],
         [fig]
     ])
-    
+
     if args.show:
         show(app)  # Save file and display in web browser.
     else:
@@ -222,9 +223,9 @@ if False:
     df_data["date"] = df_data["date"].astype(str)
 
     factor = "industry"
-    
+
     from base import growth_vars
-    df_growth = growth_vars(df_data, columns=["gva", "hours worked", "oph"], 
+    df_growth = growth_vars(df_data, columns=["gva", "hours worked", "oph"],
                             reverse="hours worked", by=factor,
                             reverse_suffix=" (sign reversed)")
 
@@ -240,11 +241,11 @@ if False:
 
     source = ColumnDataSource(df_growth)
     view_by_factor = factor_view(source, factor)
-    
+
     bar_variables = ["gva", "hours worked (sign reversed)"]
     color_map = {v: color for v, color in zip(bar_variables,
                                               palettes.Category20_3)}
-    
+
 
     vbars = grouped_stack(
         fig,
